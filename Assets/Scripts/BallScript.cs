a53e302f8ffacd67302bbfd9ccffda0d288d3205
Siddhant Bansal <bansal.siddhant@gmail.com>
2016-07-06 11:30:47 +0530

    Reorganized project

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+
+    public float force = 10f;
+    Rigidbody rb;
+    bool isRunning;
+
+	void Start () {
+        rb = GetComponentInParent<Rigidbody>();
+	}
+	
+	void Update () {
+	    
+	}
+
+    IEnumerator PushBall()
+    {
+        while (true)
+        {
+            isRunning = true;
+            rb.AddForce(Vector3.left * force);
+            yield return null;
+        }
+    }
+
+    //void OnTriggerEnter(Collider col)
+    //{
+    //    if (col.CompareTag("Hand"))
+    //    {
+    //        if(!isRunning)
+    //            StartCoroutine("PushBall");
+    //    }
+    //}
+
+    //void OnTriggerExit(Collider col)
+    //{
+    //    if(col.CompareTag("Hand"))
+    //    {
+    //        StopAllCoroutines();
+    //        isRunning = false;
+    //    }
+    //}
+}
